999ea9987b07ea3cf5c6a348d7e71ae492e9cc91
Riyadhi <faishal.riyadhi@gmail.com>
2022-06-05 23:38:20 +0800

    Challenge Js Callback

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (list, FnCall) => {
+  let arr = FnCall(list);
+
+  let hasil = [];
+  for (let index = 0; index < arr.length; index++) {
+    hasil.push(index+1 + ". " + arr[index]);
+  }
+  return hasil;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (dataArr) => {
+  return dataArr.sort();
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (dataArr) => {
+  return dataArr.sort().reverse();
+}
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
